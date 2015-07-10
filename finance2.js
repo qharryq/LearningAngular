d6894bde09976ab9230d4a2c9930dc5c86af0b7a
qharryq <qharryq@hotmail.com>
2015-07-11 00:25:49 +0100

    First commit

---
+++ finance2.js
@@ -0,0 +1,21 @@
+//service - contains logic that can be used by other modules
+(function(angular) {
+  'use strict';
+angular.module('finance2', [])
+  .factory('currencyConverter', function() {
+    var currencies = ['USD', 'EUR', 'CNY'];
+    var usdToForeignRates = {
+      USD: 1,
+      EUR: 0.74,
+      CNY: 6.09
+    };
+    var convert = function (amount, inCurr, outCurr) {
+      return amount * usdToForeignRates[outCurr] / usdToForeignRates[inCurr];
+    };
+
+    return {
+      currencies: currencies,
+      convert: convert
+    };
+  });
+})(window.angular);
